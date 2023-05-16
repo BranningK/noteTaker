f9395b5506582cfe289b047d81b12cee25fb12f5
Branning <bmk2110@gmail.com>
2023-05-16 15:38:21 -0400

    restructure 2

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const notes = require('./db/notes');
 const fs = require('fs');
-const uuid = require('./public/assets/helpers/uuid');
+const uuid = require('./assets/helpers/uuid');
 
 const app = express();
 const PORT = 3001;
